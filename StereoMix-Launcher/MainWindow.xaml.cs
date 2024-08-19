fce126ff27060e36d3c2dbd782163aecf4aa493a
LiF <leesj.dev@gmail.com>
2024-08-20 02:33:48 +0900

    SNS 버튼 추가 및 임시 다운로드 기능 추가

--- StereoMix-Launcher/MainWindow.xaml.cs
+++ StereoMix-Launcher/MainWindow.xaml.cs
@@ -1,19 +1,119 @@
 ﻿using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
 namespace StereoMix_Launcher;
 
-public partial class MainWindow
+public partial class MainWindow : Window
 {
+    private const string GamePath = "./StereoMix/StereoMix.exe";
+    private const string DownloadUrl = "https://example.com/StereoMix.zip";
+
     public MainWindow()
     {
         InitializeComponent();
         StateChanged += MainWindow_StateChanged;
+        CheckGameInstallation();
     }
-    
+
+    private void CheckGameInstallation()
+    {
+        StartButton.Content = File.Exists(GamePath) ? "게임 실행" : "게임 설치";
+    }
+
+    private void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (File.Exists(GamePath))
+        {
+            LaunchGame();
+        }
+        else
+        {
+            StartButton.IsEnabled = false;
+            DownloadLatest();
+        }
+    }
+
+    private void LaunchGame()
+    {
+        var processStartInfo = new ProcessStartInfo(GamePath);
+        try
+        {
+            Process.Start(processStartInfo);
+            Environment.Exit(0);
+        }
+        catch (Win32Exception error)
+        {
+            MessageBox.Show(error.Message);
+        }
+    }
+
+    private async void DownloadLatest()
+    {
+        string extractPath = "./StereoMix";
+        await DownloadAndExtractZip(DownloadUrl, extractPath);
+    }
+
+    private async Task DownloadAndExtractZip(string url, string extractPath)
+    {
+        string tempZipPath = Path.Combine(Path.GetTempPath(), "StereoMix.zip");
+
+        using var client = new HttpClient();
+        try
+        {
+            var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+            response.EnsureSuccessStatusCode();
+
+            var totalBytes = response.Content.Headers.ContentLength ?? 1L;
+
+            await using (var fs = new FileStream(tempZipPath, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                var contentStream = await response.Content.ReadAsStreamAsync();
+                var buffer = new byte[8192];
+                int bytesRead;
+                long totalRead = 0;
+                while ((bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    await fs.WriteAsync(buffer, 0, bytesRead);
+                    totalRead += bytesRead;
+                    UpdateProgress(totalRead, totalBytes);
+                }
+            }
+
+            ZipFile.ExtractToDirectory(tempZipPath, extractPath);
+            MessageBox.Show("다운로드 및 압축 해제가 완료되었습니다.");
+            StartButton.Content = "게임 실행";
+            StartButton.IsEnabled = true;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"오류 발생: {ex.Message}");
+            StartButton.IsEnabled = true;
+        }
+        finally
+        {
+            if (File.Exists(tempZipPath))
+            {
+                File.Delete(tempZipPath);
+            }
+        }
+    }
+
+    private void UpdateProgress(long bytesReceived, long totalBytes)
+    {
+        Dispatcher.Invoke(() =>
+        {
+            double progressPercentage = (double)bytesReceived / totalBytes * 100;
+            DownloadProgressBar.Value = progressPercentage;
+        }, DispatcherPriority.Background);
+    }
+
     private void CloseButton_Click(object sender, RoutedEventArgs e)
     {
         var storyboard = new Storyboard();
@@ -37,7 +137,7 @@ public partial class MainWindow
     {
         DragMove();
     }
-    
+
     private void MinimizeButton_Click(object sender, RoutedEventArgs e)
     {
         var storyboard = new Storyboard();
@@ -65,7 +165,7 @@ public partial class MainWindow
 
         storyboard.Begin();
     }
-    
+
     private void MainWindow_StateChanged(object? sender, EventArgs e)
     {
         if (WindowState == WindowState.Normal)
@@ -94,14 +194,13 @@ public partial class MainWindow
             storyboard.Begin();
         }
     }
-    
-    private void StartButton_Click(object sender, RoutedEventArgs e)
+
+    private void SNS_X_Button_Click(object sender, RoutedEventArgs e)
     {
-        var processStartInfo = new ProcessStartInfo("StereoMixClient.exe");
+        var url = "https://x.com/StereomixGame";
         try
         {
-            Process.Start(processStartInfo);
-            Environment.Exit(0);
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
         }
         catch (Win32Exception error)
         {
